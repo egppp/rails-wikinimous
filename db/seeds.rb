c08a252c315c5cf6be3c8258ecfe2c741394f327
Elizabeth Giraldo <elizabethgiraldopinto@gmail.com>
2020-11-24 17:33:57 +0800

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+10.times do
+  title = Faker::Book.title
+  content = Faker::Marketing.buzzwords
+  Article.create(title:title, content:content)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
